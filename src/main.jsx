3ee8a128fd9b1a3d89d39b4419ac00f0e227083b
Nindya Hapsari <nindyahapsariiljas@yahoo.com>
2023-06-17 14:39:19 +0200

    add changes to router

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App.jsx'
 import './index.css'
 import 'semantic-ui-css/semantic.min.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />
+  <BrowserRouter>
+    <Routes>
+      <Route path='/*' element={<App />} />
+    </Routes>
+  </BrowserRouter>
 )
