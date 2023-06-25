a805bddb03285d967d783d64f01f8846e45bac4e
Nindya Hapsari <38020437+nindyahapsari@users.noreply.github.com>
2023-06-25 22:55:38 +0200

    add formik for form validation and handling submission, make in the wrong branch (#5)

--- src/main.jsx
+++ src/main.jsx
@@ -6,14 +6,17 @@ import './index.css'
 import 'semantic-ui-css/semantic.min.css'
 import { ApolloProvider } from "@apollo/client";
 import { client } from './Client'
+import { AlertProvider } from './Context/alertContext.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 
   <ApolloProvider client={client}>
+        <AlertProvider>
   <BrowserRouter>
     <Routes>
       <Route path='/*' element={<App />} />
     </Routes>
   </BrowserRouter>
+      </AlertProvider>
   </ApolloProvider>
 )
