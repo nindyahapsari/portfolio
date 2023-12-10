4b5c1fec080e4352194f736c2d305d65b3bac1fe
nindya/winona <nindyahapsariiljas@yahoo.com>
2023-12-10 19:48:32 +0100

    refactor bio component and add new features

--- src/Components/Bio.tsx
+++ src/Components/Bio.tsx
@@ -1,17 +1,29 @@
-import React from 'react'
+import React from "react";
+import { Card, Icon, SegmentGroup, Feed } from "semantic-ui-react";
+import elliot from "./images/elliot.jpg";
+
+import BioFeed from "./BioComponents/BioFeed";
+
+const extra = (
+  <a>
+    <Icon name="user" />
+    16 Friends
+  </a>
+);
 
 const Bio = () => {
   return (
-    <>
-      <div>
-        <img src="" alt="profile" />
-      </div>
-      <div>
-        <h1>Hi, I'm Nindya</h1>
-        <p>Frontend Developer</p>
-      </div>
-    </>
-  )
-}
+    <div>
+      <Card
+        image={elliot}
+        header="Elliot Baker"
+        meta="Friend"
+        description="Elliot is a sound engineer living in Nashville who enjoys playing guitar and hanging with his cat."
+        extra={extra}
+      />
+      <BioFeed />
+    </div>
+  );
+};
 
-export default Bio
+export default Bio;
