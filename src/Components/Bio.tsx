b61158188fd7d706a356b1e67cf2c5db49902e4c
nindya/winona <nindyahapsariiljas@yahoo.com>
2023-12-10 18:18:21 +0100

    Add Bio component

---
+++ src/Components/Bio.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Bio = () => {
+  return (
+    <>
+      <div>
+        <img src="" alt="profile" />
+      </div>
+      <div>
+        <h1>Hi, I'm Nindya</h1>
+        <p>Frontend Developer</p>
+      </div>
+    </>
+  )
+}
+
+export default Bio
