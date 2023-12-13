274bb4b0438d093f81ac6a84a425a01e72889205
nindya/winona <nindyahapsariiljas@yahoo.com>
2023-12-13 11:55:02 +0100

    installed tailwind for styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,24 @@
-import React, { FC } from 'react'
-import { Routes, Route } from 'react-router-dom'
-import './App.css'
-import Signup from './Pages/Signup'
-import Login from './Pages/Login'
-import Dashboard from './Pages/Dashboard'
-import ListPage from './Pages/ListPage'
-import Todo from './Pages/Todo'
-import GithubRepos from './Pages/GithubRepos'
+import React, { FC } from "react";
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import Signup from "./Pages/Signup";
+import Login from "./Pages/Login";
+import Dashboard from "./Pages/Dashboard";
+import ListPage from "./Pages/ListPage";
+import Todo from "./Pages/Todo";
+import GithubRepos from "./Pages/GithubRepos";
 
-
-// process of changing design for this portfolio
-// need to work on the styling probably using css
 const App: FC = () => {
   return (
     <Routes>
-      <Route path='/' element={<Login />} />
-      <Route path='/signup' element={<Signup />} />
-      <Route path='/dashboard' element={<Dashboard />} />
+      <Route path="/" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/dashboard" element={<Dashboard />} />
       {/* <Route path='/list' element={<ListPage />} /> */}
       {/* <Route path='/todo' element={<Todo />} /> */}
-      <Route path='/githubrepos' element={<GithubRepos />} />
+      <Route path="/githubrepos" element={<GithubRepos />} />
     </Routes>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,27 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import App from './App';
-import './index.css'
-import 'semantic-ui-css/semantic.min.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./App";
+import "./index.css";
+import "semantic-ui-css/semantic.min.css";
 import { ApolloProvider } from "@apollo/client";
-import { client } from './Client'
-import { AlertProvider } from './Context/alertContext.jsx';
+import { client } from "./Client";
+import { AlertProvider } from "./Context/alertContext.jsx";
+import "./index.css";
 
-const rootElement = document.getElementById('root')
+const rootElement = document.getElementById("root");
 
-if (rootElement){
+if (rootElement) {
   ReactDOM.createRoot(rootElement).render(
     <ApolloProvider client={client}>
       <AlertProvider>
         <BrowserRouter>
           <Routes>
-            <Route path='/*' element={<App />} />
+            <Route path="/*" element={<App />} />
           </Routes>
         </BrowserRouter>
       </AlertProvider>
     </ApolloProvider>
-  )
+  );
 } else {
-  console.error('Root element not found')
+  console.error("Root element not found");
 }
